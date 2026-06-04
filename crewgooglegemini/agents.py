fcb2d80ef544fb5e7e35e93a11b1378c00089851
Falak Rana <ranafalak18@gmail.com>
2026-06-04 20:40:56 +0530

    Remove new-blog-post.md file and updated crew.py

--- crewgooglegemini/agents.py
+++ crewgooglegemini/agents.py
@@ -9,7 +9,7 @@ import os
 
 ## call the gemini models
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.5-flash",
     verbose=True,
     temperature=0.5,
     google_api_key=os.getenv("GOOGLE_API_KEY"),
--- crewgooglegemini/crew.py
+++ crewgooglegemini/crew.py
@@ -12,5 +12,5 @@ crew=Crew(
 
 ## starting the task execution process wiht enhanced feedback
 
-result=crew.kickoff(inputs={'topic':'What was the case of Nushrat Bharrucha after RCB\'s victory this year?'})
+result=crew.kickoff(inputs={'topic':'What was the main reason for 2026 victory of RCB in IPL?'})
 print(result)
